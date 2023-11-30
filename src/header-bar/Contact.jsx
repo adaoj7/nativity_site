5974a4665ed0e35a514521278b8917bb9c1813fb
Adam Johnson <adaojohnson902@gmail.com>
2023-11-30 00:56:19 -0700

    integrated initial google maps on contact page

--- src/header-bar/Contact.jsx
+++ src/header-bar/Contact.jsx
@@ -19,6 +19,7 @@ const Contact = () => {
                           <p className="flex justify-center text-lg font-semibold">Tel: 309-361-9956</p>
                         </div>
                         <iframe
+                            defer
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3023.359880354713!2d-89.65405992450053!3d40.73210603638536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880a5c39a382250d%3A0x6fcfa24cada00e1b!2sThe%20Community%20Festival%20of%20Nativities!5e0!3m2!1sen!2sus!4v1701326064004!5m2!1sen!2sus"
                             width="600"
                             height="450"
