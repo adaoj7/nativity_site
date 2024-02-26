4ab54ce44f253538d550c4f22c87dd4f459e90a4
Adam Johnson <adaojohnson902@gmail.com>
2024-02-26 16:59:14 -0700

    updated the donation page and fixed prod funcitonality

--- src/components/VolunteerComponents/Donate.jsx
+++ src/components/VolunteerComponents/Donate.jsx
@@ -1,28 +1,43 @@
-﻿import React from 'react'
-import axios from 'axios'
+﻿import React from "react";
+import axios from "axios";
 
 const createDonationSession = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-        const {data} = await axios.post('/api/create-checkout-session/donate')
-        location.replace(data)
+        const { data } = await axios.post(
+            "/api/create-checkout-session/donate"
+        );
+        location.replace(data);
     } catch (err) {
-        console.log(err)
+        console.log(err);
     }
-}
+};
 
 const Donate = () => {
-  return (
-    <div className='mt-40'>
-
-    <form onSubmit={createDonationSession}>
-        <button 
-        type='submit' 
-        className='bg-green-500 btn hover:bg-slate-500'
-        >Donate</button>
-    </form>
+    return (
+        <div className="mt-24 h-[73vh]">
+            <div className="flex h-40 font-medium text-2xl justify-center items-center">
+                Click here to be taken to a secure payment powered by Stripe
+            </div>
+            <form
+                onSubmit={createDonationSession}
+                className="flex justify-center content-center"
+            >
+                <button
+                    type="submit"
+                    className="bg-green-500 btn hover:bg-slate-500"
+                >
+                    Donate
+                </button>
+            </form>
+            <div className="flex justify-center text-lg px-60 mx-80 mt-8">
+                The Community Festival of Nativities is a recognized non-profit;
+                monetary and in-kind donations are tax-deductible. Your generous
+                contributions help us maintain this as a free event to the
+                public.
+            </div>
         </div>
-  )
-}
+    );
+};
 
-export default Donate
+export default Donate;
