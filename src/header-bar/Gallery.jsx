9be65622aebdef1731b6effbdbce7d4e88b70f04
Adam Johnson <adaojohnson902@gmail.com>
2024-03-13 15:46:02 -0600

    added gallery carousel need to implement pictures using a cloud provider

--- src/header-bar/Gallery.jsx
+++ src/header-bar/Gallery.jsx
@@ -1,12 +1,95 @@
 ﻿import React from "react";
-import NativityLogo from "../components/Elements/NativityLogo";
 
 const Gallery = () => {
     return (
         <>
-            <NativityLogo />
-            <div className="mt-24 h-[73vh]">
-                <div>Gallery</div>
+            <div className="mt-24 min-h-[73vh] flex justify-center">
+                <div className="flex justify-center flex-col w-2/3">
+                    <div className=" w-full carousel rounded-box">
+                        <div
+                            id="item1"
+                            className="carousel-item relative w-full"
+                        >
+                            <img
+                                src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg"
+                                className="w-full"
+                            />
+                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                                <a href="#item4" className="btn btn-circle">
+                                    ❮
+                                </a>
+                                <a href="#item2" className="btn btn-circle">
+                                    ❯
+                                </a>
+                            </div>
+                        </div>
+                        <div
+                            id="item2"
+                            className="carousel-item relative w-full"
+                        >
+                            <img
+                                src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
+                                className="w-full"
+                            />
+                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                                <a href="#item1" className="btn btn-circle">
+                                    ❮
+                                </a>
+                                <a href="#item3" className="btn btn-circle">
+                                    ❯
+                                </a>
+                            </div>
+                        </div>
+                        <div
+                            id="item3"
+                            className="carousel-item relative w-full"
+                        >
+                            <img
+                                src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg"
+                                className="w-full"
+                            />
+                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                                <a href="#item2" className="btn btn-circle">
+                                    ❮
+                                </a>
+                                <a href="#item4" className="btn btn-circle">
+                                    ❯
+                                </a>
+                            </div>
+                        </div>
+                        <div
+                            id="item4"
+                            className="carousel-item relative w-full"
+                        >
+                            <img
+                                src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg"
+                                className="w-full"
+                            />
+                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                                <a href="#item3" className="btn btn-circle">
+                                    ❮
+                                </a>
+                                <a href="#item1" className="btn btn-circle">
+                                    ❯
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex justify-center w-full py-2 gap-2">
+                        <a href="#item1" className="btn btn-xs">
+                            1
+                        </a>
+                        <a href="#item2" className="btn btn-xs">
+                            2
+                        </a>
+                        <a href="#item3" className="btn btn-xs">
+                            3
+                        </a>
+                        <a href="#item4" className="btn btn-xs">
+                            4
+                        </a>
+                    </div>
+                </div>
             </div>
         </>
     );
